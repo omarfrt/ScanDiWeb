adf941d6b42d59788b37cc9912d700d9500d653c
omarfrt <iroonix5@gmail.com>
2022-10-04 15:45:56 +0100

    adding react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import GlobalStyles from "./global/reset";
 import "./App.css";
-import Cart from "./pages/cart";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Switch,
+  Route,
+} from "react-router-dom"
+import CartPage from "./pages/cart";
+import CategoryPage from "./pages/category";
+import ProductPage from "./pages/pdp";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
-      <Cart/>
+      <Router>
+        <Switch>
+          <Route exact path="/product">
+            <ProductPage/>
+          </Route>
+          <Route exact path="/cart">
+            <CartPage/>
+          </Route>
+          <Route component={CategoryPage} path="/:category">
+            <CategoryPage/>
+          </Route>
+          <Route path="/">
+            <Redirect to="/tech"/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
