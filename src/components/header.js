1fc32035720d1bc4792c22ebbed66806f26ca226
omarfrt <iroonix5@gmail.com>
2022-10-06 15:26:12 +0100

    update header styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,67 @@
 import React from "react";
 import * as Typography from "./typography"
 import Styles from "../styles/header.module.css"
+import styled  from "styled-components";
 
+import { withRouter, Link} from "react-router-dom"
+
+const CATEGORIES = ["tech","clothes", "something"];
+
+const HeaderLink = styled(Typography.Small)`
+    text-transform: uppercase;
+    padding: 4px 16ps 32px;
+    color: ${({isActive})=>(isActive? "#4ECE7B": "#1D1F22")};
+    border-bottom: ${({ isActive }) => (isActive ? "2px solid #5ece7b" : "none")};
+`;
+
+const Container = styled.nav`
+position: relative;
+display: flex;
+justify-content: space-between;
+align-items: flext-start;
+`;
+
+const LinksContainer = styled.div`
+display: flex;
+gap: 16px;
+`;
+
+const Logo = styled.img`
+position: absolute;
+top:0%;
+left:50%;
+transform: translateX(-50%);
+`;
+
+const ActionsContainer= styled.div`
+display: flex;
+gap:22px;`;
 class Header extends React.Component {
     render() {
+        const categoryParam = this.props.match.params.category;
+    
         return (
-            <div className={Styles.header}>
-                <div className={Styles.header_navigation_container}>
-                    <Typography.SmallB> Women </Typography.SmallB>
-                    <Typography.Small> Men </Typography.Small>
-                    <Typography.Small> Kids </Typography.Small>
-                </div>
-                <div>
-                <img src="/a-logo.svg" alt="Logo" />
-                </div>
-                <div className={Styles.header_Action_container}>
-                   
-                    <img src="/$v.svg" alt="Currancy" />
-
-                    <img src="/cart.svg" alt="cart" />
-                    
-                </div>
-             
-            </div>
-            );
+          <Container>
+            <LinksContainer>
+              {CATEGORIES.map((category) => (
+                <Link to={`/${category}`}>
+                  <HeaderLink key={category} isActive={categoryParam === category}>
+                    {category}
+                  </HeaderLink>
+                </Link>
+              ))}
+            </LinksContainer>
+            <Logo src="/a-logo.svg" alt="Logo" />
+            <ActionsContainer>
+              <img src="/$v.svg" alt="Currency" />
+    
+              <img src="/cart.svg" alt="cart" />
+            </ActionsContainer>
+          </Container>
+        );
+      }
     }
-  }
+    
+    
 
-  export default Header;
+  export default withRouter(Header);
