0ac6cf4f31105429b79ae711976dd2b51cc52df4
omarfrt <iroonix5@gmail.com>
2022-10-12 17:06:24 +0100

    cart item final design done ✔️ 😃👍

--- src/components/cartItem.js
+++ src/components/cartItem.js
@@ -1,33 +1,93 @@
 import React from "react";
-import * as Typography from "../components/typography"
-import Styles from "../styles/cartItem.module.css"
+import * as Typography from "../components/typography";
+import styled from "styled-components";
 
-class CartItem extends React.Component{
-    render(){
-        return(
-            
-            <div className={Styles.wrapper}>
-                <div>
-                <div className={Styles.title}>
-                    <Typography.P1title> Apollo</Typography.P1title>
-                    <Typography.P2title> Running Short</Typography.P2title> 
-                    </div>
-                    <div className={Styles.price}>
-                    <Typography.Price>50.00$</Typography.Price>
-                    </div>
-                    <div className={Styles.size}>
-                    <Typography.Size>Size:</Typography.Size>
-                    </div>
-                    <div className={Styles.color}>
-                    <Typography.Size>Color:</Typography.Size>
-                    </div>
-                </div>
-                <div className={Styles.image}>
-                    <img src="./Image.png" alt="Product" width={"200px"} height={"288px"} />
-                </div>
-            </div>
-               
-        )
-    }
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  text-align: left;
+  border-top: 1px solid #e5e5e5;
+  padding-top: 24px;
+  padding-bottom: 24px;
+`;
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+const Size = styled.button`
+  width: 63px;
+  height: 45px;
+  border: 1px solid black;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: ${(props) => (props.selected ? "black" : "white")};
+  color: ${(props) => (props.selected ? "white" : "black")};
+`;
+const SizeLayout = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+`;
+const Color = styled.button`
+  width: 36px;
+  height: 36px;
+  border: ${(props) =>
+    props.selected ? "1px solid #5ECE7B" : "1px solid white"};
+`;
+
+const sizes = ["XS", "S", "M", "L", "XL"];
+const colors = ["blue", "red", "black", "white"];
+
+class CartItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { size: "", color: "" };
+  }
+  render() {
+    return (
+      <Wrapper>
+        <Layout>
+          <Typography.P1title> Apollo</Typography.P1title>
+          <Typography.P2title> Running Short</Typography.P2title>
+          <Typography.Price>50.00$</Typography.Price>
+          <Typography.Size>SIZE:</Typography.Size>
+          <SizeLayout>
+            {sizes.map((item, index) => (
+              <Size
+                key={index}
+                selected={index === this.state.size}
+                onClick={() => this.setState({ size: index }).bind(this)}
+              >
+                {item}
+              </Size>
+            ))}
+          </SizeLayout>
+          <Typography.Size>Color:</Typography.Size>
+          <SizeLayout>
+            {colors.map((item, index) => (
+              <Color
+                key={index}
+                selected={index === this.state.color}
+                onClick={() => this.setState({ color: index }).bind(this)}
+                style={{ background: `${colors[index]}` }}
+              />
+            ))}
+          </SizeLayout>
+        </Layout>
+        <>
+          <img
+            src="./Image.png"
+            alt="Product"
+            width={"200px"}
+            height={"288px"}
+          />
+        </>
+      </Wrapper>
+    );
+  }
 }
 export default CartItem;
--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,29 +1,28 @@
 import React from "react";
 import CartItem from "../components/cartItem";
 import Header from "../components/header";
-import Styles from "../styles/cart.module.css"
-import * as Typography from "../components/typography"
+import Styles from "../styles/cart.module.css";
+import * as Typography from "../components/typography";
 import CartTotal from "../components/cartTotal";
-class Cart extends React.Component{
-    render(){
-        return(
-            <div>
-                <Header/>
-                <div className={Styles.wrapper}>
-                    <div className={Styles.page_title}>
-                <Typography.H2>Cart</Typography.H2>
-                    </div>
-                <div className={Styles.cart_items}>     
-                <CartItem/>
-                <CartItem/>
-             
-                    </div>
-                <div>
-                    <CartTotal/>
-                    </div>    
-                </div>
-            </div>
-        )
-    }
+class Cart extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className={Styles.wrapper}>
+          <div className={Styles.page_title}>
+            <Typography.H2>Cart</Typography.H2>
+          </div>
+          <div className={Styles.cart_items}>
+            <CartItem />
+            <CartItem />
+          </div>
+          <div>
+            <CartTotal />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default Cart;
