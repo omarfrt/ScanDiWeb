b6d5e1f8060c101e35cd0a487995b32bf3c2c847
omarfrt <iroonix5@gmail.com>
2022-11-03 16:29:54 +0100

    image scroll ⬅️ ➡️ in cart done ✅

--- src/components/cartImage.js
+++ src/components/cartImage.js
@@ -1,17 +1,68 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 
+const Wrapper = styled.div`
+  width: 200px;
+  height: 288px;
+  position: relative;
+`;
 const Thumbnail = styled.img`
   width: 200px;
   height: 288px;
+`;
+const SliderWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 8px;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+`;
+const SliderButton = styled.button`
+  width: 24px;
+  height: 24px;
+  left: 56px;
+  top: 0px;
+  color: white;
+
+  background: rgba(0, 0, 0, 0.73);
 `;
 
 export default class CartImage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      current: 0,
+    };
   }
+  handleNextSlide = () => {
+    this.setState({
+      current:
+        this.state.current === this.props.gallery.length - 1
+          ? 0
+          : this.state.current + 1,
+    });
+  };
+  handlePrevSlide = () => {
+    this.setState({
+      current:
+        this.state.current === 0
+          ? this.props.gallery.length - 1
+          : this.state.current - 1,
+    });
+  };
   render() {
-    const imgsrc = this.props.gallery[0];
-    return <Thumbnail src={imgsrc} alt="thumbnail" />;
+    const imgsrc = this.props.gallery;
+    return (
+      <Wrapper>
+        <SliderWrapper>
+          <SliderButton onClick={this.handlePrevSlide}>{"<"}</SliderButton>
+          <SliderButton onClick={this.handleNextSlide}>{">"}</SliderButton>
+        </SliderWrapper>
+        <Thumbnail src={imgsrc[this.state.current]} alt="thumbnail" />
+      </Wrapper>
+    );
   }
 }
